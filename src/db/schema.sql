761e2bae794deeeba37da9f99e7b1a18b622b536
Danyelle <dkobes15@gmail.com>
2025-03-04 19:14:53 -0600

    moving database.js and updating database

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -8,7 +8,8 @@ CREATE TABLE role (
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id),
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -17,6 +18,8 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
     manager_id INTEGER,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id),
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id),
+    REFERENCES employee(id)
 );
